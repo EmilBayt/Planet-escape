318f35301ebcc4202278e251a67a105f3e7ccd7e
baitemirov <emil.baitemirov@dfki.de>
2022-02-23 18:59:11 +0100

    First commit

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+
+    Rigidbody rigidbody;
+    [SerializeField] float thrustSpeed = 100f;
+    [SerializeField] float rotationSpeed = 100f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessThrust();
+        ProcessRotation();
+    }
+
+    
+    void ProcessThrust() 
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rigidbody.AddRelativeForce(Vector3.up * Time.deltaTime * thrustSpeed);
+        }
+    }
+
+    void ProcessRotation()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            ApplyRotation(rotationSpeed);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            ApplyRotation(-rotationSpeed);
+        }
+    }
+
+    void ApplyRotation(float rotationThisFrame)
+    {
+        rigidbody.freezeRotation = true; //freeze rotation
+        transform.Rotate(Vector3.forward * Time.deltaTime * rotationThisFrame);
+        rigidbody.freezeRotation = false; //unfreeze rotation
+    }
+}
